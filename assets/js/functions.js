f2b7ff2d4dfbbe4f75c19007c06a11c29e7c6c12
Jakub Świadek <jakubswiadek@me.com>
2016-12-26 20:34:53 +0100

    Removed code for shareable bits of text

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -23,24 +23,3 @@ function togglePlayIcon() {
 		document.getElementById("pause-button").style.display = "none"
 	}
 }
-
-var tweetables = document.getElementsByClassName("tweetable")
-var tweetablesLength = tweetables.length
-
-for (var i = 0; i < tweetablesLength; i++) {
-	tweetables[i].setAttribute("href", "https://twitter.com/intent/tweet?text=%22" + tweetables[i].innerHTML + "%22 - read full story at&url=" + window.location.href)
-	
-	var tweetIcon = document.createElement("span")
-	tweetIcon.setAttribute("class", "tweet-icon")
-	
-	var dimmer = document.createElement("span")
-	dimmer.setAttribute("class", "dimmer")
-	
-	tweetables[i].insertBefore(tweetIcon, tweetables[i].childNodes[0])
-	tweetables[i].insertBefore(dimmer, tweetables[i].childNodes[0])
-}
-
-/* Footer share link setup --------------------------- */
-
-document.getElementById("twitter-share").setAttribute("href", "https://twitter.com/intent/tweet?url=" + window.location.href)
-document.getElementById("facebook-share").setAttribute("href", "https://www.facebook.com/sharer.php?u=" + window.location.href)
